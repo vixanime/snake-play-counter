658e82345ba83fa8dc80bbb60b2ec1243b68b794
lix <rojratu@gmail.com>
2025-06-07 04:48:56 +0800

    Add visitcount endpoint

--- api/visitcount.js
+++ api/visitcount.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const SUPABASE_URL = 'https://rstnrqtlydnuolufcuby.supabase.co';
-  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzdG5ycXRseWRudW9sdWZjdWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2Mjk5OTgsImV4cCI6MjA2NDIwNTk5OH0.HNFCMkQDAyCPgr4tZe5h9XAx8COVSw-M-DIGwOwQ4_g';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzdG5ycXRseWRudW9sdWZjdWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2Mjk5OTgsImV4cCI6MjA2NDIwNTk5OH0.HNFCMkQDAyCPgr4tZe5h9XAx8COVSw-M-DIGwOwQ4_gE';
 
   const response = await fetch(`${SUPABASE_URL}/rest/v1/snake_game_stats?id=eq.1&select=visits`, {
     headers: {
